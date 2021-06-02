c05fe22e41b51b228d57a335596f377126a209b2
Liangyue Zhao <liangyuezhao7@gmail.com>
2021-06-02 17:40:03 -0400

    Added GLFW

---
+++ cmake/FindGLFW.cmake
@@ -0,0 +1,33 @@
+find_path(
+    GLFW_INCLUDE_PATH
+    NAMES "GLFW/glfw3.h"
+    PATHS "${CMAKE_SOURCE_DIR}/libs/GLFW/include"
+)
+
+if (WIN32)
+    if (MSVC)
+        find_library(
+            GLFW_LIBRARY
+            NAMES "glfw3.lib" "glfw3dll.lib"
+            PATHS "${CMAKE_SOURCE_DIR}/libs/GLFW/lib" "${CMAKE_SOURCE_DIR}/libs/GLFW/src" 
+                  "${CMAKE_SOURCE_DIR}/libs/GLFW/build/src"
+        )
+    else()
+        find_library(
+            GLFW_LIBRARY
+            NAMES "libglfw3.a" "libglfw3dll.a"
+            PATHS "${CMAKE_SOURCE_DIR}/libs/GLFW/lib" "${CMAKE_SOURCE_DIR}/libs/GLFW/src" 
+                  "${CMAKE_SOURCE_DIR}/libs/GLFW/build/src"
+        )
+    endif()
+else()
+    find_library(
+        GLFW_LIBRARY
+        NAMES "libglfw3.a" "libglfw.so"
+        PATHS "${CMAKE_SOURCE_DIR}/libs/GLFW/lib" "${CMAKE_SOURCE_DIR}/libs/GLFW/src" 
+              "${CMAKE_SOURCE_DIR}/libs/GLFW/build/src"
+    )
+endif()
+
+include("FindPackageHandleStandardArgs")
+find_package_handle_standard_args("GLFW" DEFAULT_MSG GLFW_LIBRARY GLFW_INCLUDE_PATH)
