4789269af002801d0cf29f189f58679ebc502510
Liangyue Zhao <liangyuezhao7@gmail.com>
2021-06-02 17:55:32 -0400

    Started working on the Engine

---
+++ include/3d-game-engine/Engine.hpp
@@ -0,0 +1,20 @@
+#ifndef FB64FE50_90D1_4B51_A3E8_B647A2366A2E
+#define FB64FE50_90D1_4B51_A3E8_B647A2366A2E
+
+namespace n3DGE {
+    class Engine {
+    private:
+        
+    public:
+        // Constructor, which marks the start of the engine
+        Engine();
+        
+        // Update function, which runs every frame
+        void update();
+        
+        // Destructor, which destroys the program and concludes it
+        ~Engine();
+    };
+}
+
+#endif /* FB64FE50_90D1_4B51_A3E8_B647A2366A2E */
