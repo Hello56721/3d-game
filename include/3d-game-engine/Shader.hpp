e7b789cffb312bb618a280ad01d4a2555053e9d0
Liangyue Zhao <liangyuezhao7@gmail.com>
2021-06-06 17:49:34 -0400

    Started on the Shader class

---
+++ include/3d-game-engine/Shader.hpp
@@ -0,0 +1,16 @@
+#ifndef F35E73B2_4E76_4C71_A844_760CC650F090
+#define F35E73B2_4E76_4C71_A844_760CC650F090
+
+namespace n3DGE {
+    class Shader {
+    private:
+        // The handle to the shader object in OpenGL
+        unsigned int shader;
+        
+    public:
+        // Default Constructor
+        Shader();
+    }
+}
+
+#endif /* F35E73B2_4E76_4C71_A844_760CC650F090 */
