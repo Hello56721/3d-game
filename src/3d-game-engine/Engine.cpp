4cf084536626f98d1a2f4140435e1774d68550ed
Liangyue Zhao <liangyuezhao7@gmail.com>
2021-06-08 15:13:11 -0400

    Added a FPS measure/

--- src/3d-game-engine/Engine.cpp
+++ src/3d-game-engine/Engine.cpp
@@ -3,11 +3,16 @@
 #include <iostream>
 #include <3d-game-engine/Window.hpp>
 #include "OpenGLDebug.hpp"
+#include <string>
 
 using namespace n3DGE;
 
 extern Window* window;
 Window* window = nullptr;
+
+struct {
+    float deltaTime;
+} Time;
 
 n3DGE::Engine::Engine() {
     window = new Window();
@@ -17,7 +22,14 @@ n3DGE::Engine::Engine() {
 }
 
 void n3DGE::Engine::update() {
+    float startTime = glfwGetTime();
+    
     window->update();
+    
+    float endTime = glfwGetTime();
+    Time.deltaTime = endTime - startTime;
+    
+    window->setTitle(std::string("3D Game\tFPS: ") + std::to_string(static_cast<unsigned int>(1.0 / Time.deltaTime)));
 }
 
 n3DGE::Engine::~Engine() {
